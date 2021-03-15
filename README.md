cb99016b9483838f6084da5333ae723301c8acf2
AnisaSuljic <AnisaSuljic@users.noreply.github.com>
2021-03-16 00:14:02 +0100

    Vjezbe 1 (Kreiranje projekta, upoznavanje sa servisima, interfejsima servisa i koristenje swaggera)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RS2-2021
 Vježbe iz predmeta RS2-2021
+
+*Swagger 
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-5.0&tabs=visual-studio
+
+
---
+++ eProdaja/eProdaja/Services/ProizvodService.cs
@@ -0,0 +1,56 @@
+﻿using eProdaja.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodService:IProizvodService
+    {
+        static List<Proizvod> _proizvodi;
+
+        static ProizvodService()
+        {
+            _proizvodi = new List<Proizvod>()
+            {
+                new Proizvod()
+                {
+                    Id=1,
+                    Name="Laptop"
+                },
+                new Proizvod()
+                {
+                    Id=2,
+                    Name="Miš"
+                },
+                new Proizvod()
+                {
+                    Id=3,
+                    Name="Tastatura"
+                }
+            };
+        }
+
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvodi;
+        }
+        public Proizvod GetById(int id)
+        {
+            return _proizvodi.Where(p => p.Id == id).FirstOrDefault();
+        }
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            _proizvodi.Add(proizvod);
+            return proizvod;
+        }
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            var trenutno = _proizvodi.Where(p => p.Id == id).FirstOrDefault();
+            trenutno.Name = proizvod.Name;
+
+            return trenutno;
+        }
+    }
+}
