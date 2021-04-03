baf1ba919c835dc55a1b60ded9157bac1ad3edf3
AnisaSuljic <62303675+AnisaSuljic@users.noreply.github.com>
2021-04-03 23:37:16 +0200

    vjezbe3

---
+++ eProdaja/eProdaja/Controllers/ProizvodiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eProdaja.Model.Requests;
+using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : BaseCRUDController<Model.Proizvodi, Model.ProizvodiSearchObject,ProizvodiInsertRequest,ProizvodiUpdateRequest>
+    {
+        public ProizvodiController(IProizvodiService service)
+            : base(service)
+        {
+        }
+    }
+}
