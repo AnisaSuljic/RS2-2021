c8764ad22e5d4953bd53b6afec44cfa711c1b277
AnisaSuljic <62303675+AnisaSuljic@users.noreply.github.com>
2021-05-07 16:24:07 +0200

    v7
    uradjene migracije (fali dio za docker)

--- eProdaja/eProdaja/Program.cs
+++ eProdaja/eProdaja/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eProdaja.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +15,13 @@ namespace eProdaja
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var database = scope.ServiceProvider.GetService<eProdajaContext>();
+                new SetupService().Init(database);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
